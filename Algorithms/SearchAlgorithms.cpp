edaee32a315ddcfb265413c57c109ac201d76560
adar2 <warmundadar@gmail.com>
2020-12-06 17:17:03 +0200

    new repo

--- Algorithms/SearchAlgorithms.cpp
+++ Algorithms/SearchAlgorithms.cpp
@@ -7,7 +7,7 @@
 #include <algorithm>
 using namespace std;
 
-struct Node{
+class Node{
 private:
     float cost;
     int row;
@@ -18,21 +18,21 @@ public:
     float getCost(){return cost;};
     int getRow(){return row;};
     int getCol(){return col;};
-    bool operator==(const Node &node){return this->row == node.row && this->col == node.col;};
-    friend  bool operator<(const Node &node1,const Node &node2);
+    friend bool operator==(const Node &node1,const Node &node2);
+    friend bool operator>(const Node &node1,const Node &node2);
 };
 
-bool operator<(const Node &node1,const Node &node2){return node1.cost < node2.cost;}
-
+bool operator>(const Node &node1,const Node &node2){return node1.cost > node2.cost;}
+bool operator==(const Node &node1,const Node &node2){return node1.row == node2.row && node1.col == node2.col;}
 
 int uniformCostSearch(int **array, int dimension,int *start, int *target){
     int pathCost = 0 ;
     vector<string> path = vector<string>();
-    vector<struct Node> explored = vector<struct Node>();
-    priority_queue<struct Node> openList = priority_queue<struct Node>();
-    struct Node sourceNode = Node(0, start[0], start[1]);
-    struct Node goalNode = Node(0, target[0], target[1]);
-    struct Node current_node;
+    vector<Node> explored = vector<Node>();
+    priority_queue<Node,vector<Node>,greater<Node>> openList = priority_queue<Node,vector<Node>,greater<Node>>();
+    Node sourceNode = Node(0, start[0], start[1]);
+    Node goalNode = Node(0, target[0], target[1]);
+    Node current_node;
     openList.push(sourceNode);
     while (!openList.empty()){
          current_node = openList.top();
@@ -43,50 +43,50 @@ int uniformCostSearch(int **array, int dimension,int *start, int *target){
          explored.push_back(current_node);
          //UP
          if(current_node.getRow() - 1 >= 0 && array[current_node.getRow() -1][current_node.getCol()] >=0){
-             struct Node node = Node(array[current_node.getRow() -1][current_node.getCol()],current_node.getRow() -1,current_node.getCol());
+             Node node = Node(array[current_node.getRow() -1][current_node.getCol()],current_node.getRow() -1,current_node.getCol());
              if(!count(explored.begin(),explored.end(),node))
                 openList.push(node);
          }
 
          //UP-RIGHT
         if(current_node.getRow() - 1 >= 0 && current_node.getCol() + 1 < dimension && array[current_node.getRow() -1][current_node.getCol() +1] >=0){
-            struct Node node = Node(array[current_node.getRow() -1][current_node.getCol() +1],current_node.getRow() -1,current_node.getCol() +1);
+            Node node = Node(array[current_node.getRow() -1][current_node.getCol() +1],current_node.getRow() -1,current_node.getCol() +1);
             if(!count(explored.begin(),explored.end(),node))
                 openList.push(node);
         }
          //RIGHT
         if(current_node.getCol() + 1 < dimension && array[current_node.getRow()][current_node.getCol() +1] >= 0){
-            struct Node node = Node(array[current_node.getRow()][current_node.getCol() +1],current_node.getRow(),current_node.getCol()+1);
+            Node node = Node(array[current_node.getRow()][current_node.getCol() +1],current_node.getRow(),current_node.getCol()+1);
             if(!count(explored.begin(),explored.end(),node))
                 openList.push(node);
         }
          //DOWN-RIGHT
         if(current_node.getRow() + 1 < dimension && current_node.getCol() +1 <dimension && array[current_node.getRow() + 1][current_node.getCol() +1] >= 0){
-            struct Node node = Node(array[current_node.getRow() + 1][current_node.getCol() +1],current_node.getRow() +1,current_node.getCol() +1);
+            Node node = Node(array[current_node.getRow() + 1][current_node.getCol() +1],current_node.getRow() +1,current_node.getCol() +1);
             if(!count(explored.begin(),explored.end(),node))
                 openList.push(node);
         }
          //DOWN
         if(current_node.getRow() + 1 < dimension && array[current_node.getRow() + 1][current_node.getCol()] >= 0){
-            struct Node node = Node(array[current_node.getRow() + 1][current_node.getCol()],current_node.getRow() +1,current_node.getCol());
+            Node node = Node(array[current_node.getRow() + 1][current_node.getCol()],current_node.getRow() +1,current_node.getCol());
             if(!count(explored.begin(),explored.end(),node))
                 openList.push(node);
         }
          //DOWN-LEFT
         if(current_node.getCol() - 1 >= 0 && current_node.getRow() +1 <dimension && array[current_node.getRow() +1][current_node.getCol() -1] >= 0){
-            struct Node node = Node(array[current_node.getRow() +1][current_node.getCol() -1],current_node.getRow() +1,current_node.getCol() -1);
+            Node node = Node(array[current_node.getRow() +1][current_node.getCol() -1],current_node.getRow() +1,current_node.getCol() -1);
             if(!count(explored.begin(),explored.end(),node))
                 openList.push(node);
         }
          //LEFT
         if(current_node.getCol() - 1 >= 0 && array[current_node.getRow()][current_node.getCol() -1] >= 0){
-            struct Node node = Node(array[current_node.getRow()][current_node.getCol() -1],current_node.getRow(),current_node.getCol() -1);
+            Node node = Node(array[current_node.getRow()][current_node.getCol() -1],current_node.getRow(),current_node.getCol() -1);
             if(!count(explored.begin(),explored.end(),node))
                 openList.push(node);
         }
          //UP-LEFT
         if(current_node.getRow() - 1 >= 0 && current_node.getCol() - 1 >= 0  && array[current_node.getRow() -1][current_node.getCol() - 1] >= 0){
-            struct Node node = Node(array[current_node.getRow() -1][current_node.getCol() - 1],current_node.getRow() -1,current_node.getCol() -1);
+            Node node = Node(array[current_node.getRow() -1][current_node.getCol() - 1],current_node.getRow() -1,current_node.getCol() -1);
             if(!count(explored.begin(),explored.end(),node))
                 openList.push(node);
         }
